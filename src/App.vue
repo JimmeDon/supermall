cbad9c264eaa4390ecb0fb692d294095f701dde2
CristianD <2305391717@qq.com>
2020-12-16 19:24:08 +0800

    '初始化项目1'

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+    props: {
+      msg: String
+    }
+  }
+</script>
+
+<style>
+
+</style>
